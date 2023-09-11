23b8615ccd4ba2fdd81f8c0d1995e4199ebb3215
Blaine Jones <blaine.g.jones-1@ou.edu>
2023-09-11 14:56:08 -0500

    created product method

--- Assignment3/src/App.java
+++ Assignment3/src/App.java
@@ -4,12 +4,26 @@ public class App {
         int[] listOfIntegers = {1, 2, 3, 4, 5};
         int sumOfIntegers = sum(listOfIntegers);
         System.out.println(sumOfIntegers);
+
+        int productOfIntegers = product(listOfIntegers);
+        System.out.println(productOfIntegers);
+
+
+
     }
 
     public static int sum(int[] addends) {
         int result = 0;
         for (int i = 0; i < addends.length; i++) {
             result += addends[i];
+        }
+        return result;
+    }
+
+    public static int product(int[] factors) {
+        int result = 1;
+        for (int i = 0; i < factors.length; i++) {
+            result *= factors[i];
         }
         return result;
     }
