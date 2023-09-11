b98595290b3a1d1ae2b92fbcf185fe18be322cca
Blaine Jones <blaine.g.jones-1@ou.edu>
2023-09-11 15:35:03 -0500

    added user input to main method

--- Assignment3/src/App.java
+++ Assignment3/src/App.java
@@ -1,29 +1,44 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) throws Exception {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter a list of numbers separated by spaces, press enter when list is complete.");
+
         
-        int[] listOfIntegers = {1, 2, 3, 4, 5};
+        
+        String[] input = in.nextLine().split(" ");
+    
+        ArrayList<Integer> listOfIntegers = new ArrayList<Integer>();
+        for (int i = 0; i < input.length; i++) {
+            int inputInt = Integer.parseInt(input[i]);
+            listOfIntegers.add(i, inputInt);
+        }
         int sumOfIntegers = sum(listOfIntegers);
-        System.out.println(sumOfIntegers);
+        System.out.println("Sum: " + sumOfIntegers);
 
         int productOfIntegers = product(listOfIntegers);
-        System.out.println(productOfIntegers);
+        System.out.println("Product: " + productOfIntegers);
 
-
+        in.close();
 
     }
 
-    public static int sum(int[] addends) {
+    public static int sum(ArrayList<Integer> addends) {
         int result = 0;
-        for (int i = 0; i < addends.length; i++) {
-            result += addends[i];
+        for (int i = 0; i < addends.size(); i++) {
+            result += addends.get(i);
         }
         return result;
     }
 
-    public static int product(int[] factors) {
+    public static int product(ArrayList<Integer> factors) {
         int result = 1;
-        for (int i = 0; i < factors.length; i++) {
-            result *= factors[i];
+        for (int i = 0; i < factors.size(); i++) {
+            result *= factors.get(i);
         }
         return result;
     }
